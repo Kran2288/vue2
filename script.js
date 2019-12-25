ca9655c732f7af740e71b44603af8e94688ea84e
Ayrat <56065447+Kran2288@users.noreply.github.com>
2019-12-25 14:12:51 +0300

    4Save

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var n = new Vue({
-            el: '#vue',
-            data: {
-                message: 'Подкинули, Закинули v-bind)',
-                name: '',
-                surname: '',
-                counter: 0,
-                students: []
-            },
+    el: '#vue',
+    data: {
+        message: 'Подкинули, Закинули v-bind)',
+        name: '',
+        surname: '',
+        counter: 0,
+        students: []
+    },
+    methods: {
+        onSubmit() {
+            this.students.push({
+                'sName': this.name + ' ' + this.surname,
+            })
+        }
+    }
+})
