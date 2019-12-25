3d18f9994e1dabab19f5518f7b050c3b27db3e1a
Ayrat <56065447+Kran2288@users.noreply.github.com>
2019-12-25 14:11:19 +0300

    3save

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var n = new Vue({
-    el: '#vue',
-	data: {
+            el: '#vue',
+            data: {
+                message: 'Подкинули, Закинули v-bind)',
+                name: '',
+                surname: '',
+                counter: 0,
+                students: []
+            },
